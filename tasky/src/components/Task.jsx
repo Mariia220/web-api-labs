7cb66dd44a78f0eb38b15e1bd52f69b20b63fe86
Mariia Popovych <20107971@setu.ie>
2026-04-15 16:20:54 +0100

    Mongo lab Integrated Tasky React App

---
+++ tasky/src/components/Task.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Task = (props) => {
+    
+       return (
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+            <p className="title">{props.title}</p>
+            <p> Due: {props.deadline}</p>
+            <p className="description">{props.description}</p>
+            <p className={`level ${props.level}`}>{props.level}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button onClick={props.deleteTask}className='deleteButton'>Delete</button>
+
+        </div>
+    )
+
+}
+
+export default Task;

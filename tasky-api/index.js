c47683aad4650c9d460729d947c93b83e70b94b8
Mariia Popovych <20107971@setu.ie>
2026-04-15 16:25:35 +0100

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -4,6 +4,8 @@ import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import usersRouter from './api/users';
+
 
 dotenv.config();
 
@@ -30,6 +32,10 @@ app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 
 app.use(errHandler);
+
+//Users router
+app.use('/api/users', usersRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
